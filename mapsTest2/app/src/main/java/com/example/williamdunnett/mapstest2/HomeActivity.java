d69e75df95109fe64ee0f80cd5e74451a6044952
William Dunnett <up719810@myport.ac.uk>
2017-12-18 16:42:52 +0000

    #1 - splash screen finished for first draft

--- mapsTest2/app/src/main/java/com/example/williamdunnett/mapstest2/HomeActivity.java
+++ mapsTest2/app/src/main/java/com/example/williamdunnett/mapstest2/HomeActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -12,6 +14,7 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_home);
 
         //Code for splash screen
