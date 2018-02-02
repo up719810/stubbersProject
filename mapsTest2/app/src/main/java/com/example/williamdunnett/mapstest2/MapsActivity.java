65f98d66fd30345dff0eb4de90adfb949c66944c
William Dunnett <up719810@myport.ac.uk>
2018-02-02 12:51:36 +0000

    #3 - dialog is now working, need to get shared prefs working inside it

--- mapsTest2/app/src/main/java/com/example/williamdunnett/mapstest2/MapsActivity.java
+++ mapsTest2/app/src/main/java/com/example/williamdunnett/mapstest2/MapsActivity.java
@@ -1,6 +1,7 @@
 package com.example.williamdunnett.mapstest2;
 
 import android.Manifest;
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
@@ -20,6 +21,7 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
@@ -55,16 +57,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private GoogleMap mMap;
     private LocationManager locationManager;
     private static final int REQUEST_LOCATION = 1;
-    private Button get_loc_btn, newUserbtn;
-    private EditText name;
-    private Spinner typeMovement, sizeGroup;
+    private Button get_loc_btn;
     public Criteria criteria;
     public String bestProvider;
     RequestQueue requestQueue;
     private ProgressBar spinner;
 
 
-    String newUserIDURL = "http://10.128.116.120/tutorial/newUserID.php";
+    String newUserIDURL = "http://10.128.116.10/tutorial/newUserID.php";
 
 
     @Override
@@ -93,18 +93,153 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
         });
 
-        spinner = (ProgressBar)findViewById(R.id.progressBar1);
-        name = (EditText)findViewById(R.id.insertName);
-        sizeGroup = (Spinner) findViewById(R.id.insert_group_size);
-        typeMovement = (Spinner) findViewById(R.id.insertMovement);
+
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        /*
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+         */
+
+       // getLocation();
+    }
+
+    public class ViewDialog {
+
+        public void showDialog(Activity activity, String msg) {
+
+            //TODO get shared prefs working
+            
+            final Dialog dialog = new Dialog(activity);
+            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+            dialog.setCancelable(false);
+            dialog.setContentView(R.layout.new_user_dialog);
+
+            final EditText name = (EditText)findViewById(R.id.insertName);
+            final Spinner typeMovement = (Spinner) findViewById(R.id.insertMovement);
+            final Spinner sizeGroup = (Spinner) findViewById(R.id.insert_group_size);
+            final ProgressBar spinner = (ProgressBar)findViewById(R.id.progressBar1);
+            final Button newUserbtn = (Button)  findViewById(R.id.newUserbtn);
+           // final SharedPreferences prefs = this.getSharedPreferences(
+                    //"com.example.williamdunnett.mapstest2", Context.MODE_PRIVATE);
+
+            Button newUserBtn = (Button) dialog.findViewById(R.id.newUserbtn);
+
+            newUserBtn.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    final String nameUser, sizeGroupUser, typeMovementUser;
+                    // Do something in response to newUserbtn click
+                    Log.d("onStart", "newUserbtn pressed");
+                    dialog.dismiss();
+                    spinner.setVisibility(View.VISIBLE);
+                    nameUser = name.getText().toString();
+                    sizeGroupUser = String.valueOf(sizeGroup.getSelectedItem());
+                    typeMovementUser = String.valueOf(typeMovement.getSelectedItem());
 
 
-        newUserbtn = (Button)  findViewById(R.id.newUserbtn);
+                    StringRequest jsonObjRequest = new StringRequest(Request.Method.POST,
+                            newUserIDURL,
+                            new Response.Listener<String>() {
+                                @Override
+                                public void onResponse(String response) {
+                                    Log.d("Result", "response: " + response);
+
+                                    try {
+                                        JSONObject obj = new JSONObject(response);
+                                        //Log.d("Result", obj.toString());
+                                        int resultID = obj.getInt("userID");    //result is key for which you need to retrieve data
+                                        Log.d("Result", "userID: " + resultID);
+                                        //prefs.edit().putInt("usersID", resultID).apply();
+                                        //TODO store userID in userPrefs for use in post coordinates
+                                    } catch (Throwable t) {
+                                        Log.e("My App", "Could not parse malformed JSON: \"" + response + "\"");
+                                    }
+
+                                    spinner.setVisibility(View.GONE);
+                                    requestQueue.stop();
+                                }
+                            }, new Response.ErrorListener() {
+
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Log.d("Result", "something went wrong");
+                            spinner.setVisibility(View.GONE);
+                            requestQueue.stop();
+                        }
+                    }) {
+
+                        @Override
+                        public String getBodyContentType() {
+                            return "application/x-www-form-urlencoded; charset=UTF-8";
+                        }
+
+                        @Override
+                        protected Map<String, String> getParams() throws AuthFailureError {
+
+
+                            Map<String, String> postParam = new HashMap<String, String>();
+
+                            postParam.put("usersName",nameUser);
+                            postParam.put("sizeOfParty",sizeGroupUser);
+                            postParam.put("typeOfUser", typeMovementUser);
+
+                            return postParam;
+                        }
+
+                    };
+
+                    requestQueue.add(jsonObjRequest);
+
+                }
+            });
+            dialog.show();
+
+        }
+    }
+
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+
+        ViewDialog alertDialoge = new ViewDialog();
+        alertDialoge.showDialog(this, "PUT DIALOG TITLE");
+
+        /*
+        final Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.new_user_dialog);
+        dialog.setTitle("Dialog box");
+
+        final EditText name = (EditText)findViewById(R.id.insertName);
+        final Spinner typeMovement = (Spinner) findViewById(R.id.insertMovement);
+        final Spinner sizeGroup = (Spinner) findViewById(R.id.insert_group_size);
+        final ProgressBar spinner = (ProgressBar)findViewById(R.id.progressBar1);
+        final Button newUserbtn = (Button)  findViewById(R.id.newUserbtn);
+        final SharedPreferences prefs = this.getSharedPreferences(
+                "com.example.williamdunnett.mapstest2", Context.MODE_PRIVATE);
+
         newUserbtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 final String nameUser, sizeGroupUser, typeMovementUser;
                 // Do something in response to newUserbtn click
-                Log.d("onCreate", "newUserbtn pressed");
+                Log.d("onStart", "newUserbtn pressed");
+                dialog.dismiss();
                 spinner.setVisibility(View.VISIBLE);
                 nameUser = name.getText().toString();
                 sizeGroupUser = String.valueOf(sizeGroup.getSelectedItem());
@@ -164,56 +299,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
                 requestQueue.add(jsonObjRequest);
 
-                }
-        });
-
-
-
-
-
-    }
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-
-        /*
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-         */
-
-       // getLocation();
-    }
-
-    @Override
-    protected void onStart()
-    {
-        super.onStart();
-
-        final Dialog dialog = new Dialog(this);
-        dialog.setContentView(R.layout.new_user_dialog);
-        dialog.setTitle("Dialog box");
-
-        Button button = (Button) dialog.findViewById(R.id.newUserbtn);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                dialog.dismiss();
             }
         });
 
+
         dialog.show();
+        */
     }
 
 
