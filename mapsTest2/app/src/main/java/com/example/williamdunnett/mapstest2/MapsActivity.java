5acfed70ed00fdf1662512e5de5a80e9409386d7
William Dunnett <up719810@myport.ac.uk>
2018-02-09 11:25:40 +0000

    #3 - get location is working, need to get server running and fix name bug on new user

--- mapsTest2/app/src/main/java/com/example/williamdunnett/mapstest2/MapsActivity.java
+++ mapsTest2/app/src/main/java/com/example/williamdunnett/mapstest2/MapsActivity.java
@@ -11,6 +11,7 @@ import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.location.Criteria;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Handler;
@@ -51,21 +52,24 @@ import org.json.JSONObject;
 import java.security.Provider;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private LocationManager locationManager;
     private static final int REQUEST_LOCATION = 1;
-    private Button get_loc_btn;
+    private Button get_loc_btn, update_location_btn;
     public Criteria criteria;
     public String bestProvider;
     RequestQueue requestQueue;
-    private ProgressBar spinner;
+    public ProgressBar spinner;
+    public Timer timer1;
 
 
-    String newUserIDURL = "http://10.128.116.251/tutorial/newUserID.php";
-    String updateUserLocationURL = "http://10.128.116.251/tutorial/postCoordinates.php";
+    String newUserIDURL = "http://10.128.116.232/tutorial/newUserID.php";
+    String updateUserLocationURL = "http://10.128.116.232/tutorial/postCoordinates.php";
 
 
     @Override
@@ -89,87 +93,21 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 Log.d("onCreate", "button pressed");
                 getLocation();
 
-                updateUsersLocation(getApplicationContext());
-
 
             }
         });
 
-
-    }
-
-    public void updateUsersLocation(Context context){
-
-        requestQueue.getCache().clear();
-
-        final SharedPreferences prefs = context.getSharedPreferences(
-                "com.example.williamdunnett.mapstest2", Context.MODE_PRIVATE);
-
-        final int userID = prefs.getInt("userID",0);
-        final String lattitude = prefs.getString("lattitude", null);
-        final String longitude = prefs.getString("longitude", null);
-
-        Log.d("updateUsersLocation", "called");
-        Log.d("userID", String.valueOf(userID));
-        Log.d("lattitude", lattitude);
-        Log.d("longitude", longitude);
-
-
-        StringRequest jsonObjRequest = new StringRequest(Request.Method.POST,
-                updateUserLocationURL,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        Log.d("Result", "response: " + response);
-
-                        try {
-                            JSONObject obj = new JSONObject(response);
-                            //Log.d("Result", obj.toString());
-
-                        } catch (Throwable t) {
-                            Log.e("My App", "Could not parse malformed JSON: \"" + response + "\"");
-                        }
-
-                        spinner.setVisibility(View.GONE);
-                        requestQueue.stop();
-                    }
-                }, new Response.ErrorListener() {
-
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.d("Result", "something went wrong");
-                spinner.setVisibility(View.GONE);
-                requestQueue.stop();
+        update_location_btn  = (Button)  findViewById(R.id.button2);
+        update_location_btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+                Log.d("onCreate", "button pressed");
+                UpdateLocation updateLocationServer = new UpdateLocation();
+                updateLocationServer.updateServer(MapsActivity.this);
 
             }
-        }) {
+        });
 
-            @Override
-            public String getBodyContentType() {
-                return "application/x-www-form-urlencoded; charset=UTF-8";
-            }
-
-            @Override
-            protected Map<String, String> getParams() throws AuthFailureError {
-
-                //TODO This is not getting called for some reason
-                Log.d("getParams update coord", "called");
-                Map<String, String> postParam = new HashMap<String, String>();
-
-                Log.d("userID", String.valueOf(userID));
-                Log.d("lattitude", lattitude);
-                Log.d("longitude", longitude);
-
-                postParam.put("usersID",String.valueOf(userID));
-                postParam.put("lattitude",lattitude);
-                postParam.put("longitude", longitude);
-
-                return postParam;
-            }
-
-        };
-
-        requestQueue.add(jsonObjRequest);
 
     }
 
@@ -196,6 +134,88 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
        // getLocation();
     }
 
+    public class UpdateLocation {
+
+        public void updateServer(final Activity activity){
+
+            final RequestQueue requestQueue1 = Volley.newRequestQueue(getApplicationContext());
+
+           // requestQueue.getCache().clear();
+
+            final SharedPreferences prefs = activity.getSharedPreferences(
+                    "com.example.williamdunnett.mapstest2", Context.MODE_PRIVATE);
+
+            final int userID = prefs.getInt("userID",0);
+            final String lattitude = prefs.getString("lattitude", null);
+            final String longitude = prefs.getString("longitude", null);
+
+            Log.d("updateUsersLocation", "called");
+            Log.d("userID", String.valueOf(userID));
+            Log.d("lattitude", lattitude);
+            Log.d("longitude", longitude);
+
+
+            StringRequest jsonObjRequest = new StringRequest(Request.Method.POST,
+                    updateUserLocationURL,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            Log.d("Result updateuserLocation", "response: " + response);
+
+                            try {
+                                JSONObject obj = new JSONObject(response);
+                                Log.d("Result", obj.toString());
+
+                            } catch (Throwable t) {
+                                Log.e("My App", "Could not parse malformed JSON: \"" + response + "\"");
+                            }
+
+                            //spinner.setVisibility(View.GONE);
+                            requestQueue1.stop();
+                        }
+                    }, new Response.ErrorListener() {
+
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.d("Result", "something went wrong");
+                   // spinner.setVisibility(View.GONE);
+                    requestQueue1.stop();
+
+                }
+            }) {
+
+                @Override
+                public String getBodyContentType() {
+                    return "application/x-www-form-urlencoded; charset=UTF-8";
+                }
+
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+
+                    //TODO This is not getting called for some reason
+                    Log.d("getParams update coord", "called");
+                    Map<String, String> postParam = new HashMap<String, String>();
+
+                    Log.d("userID", String.valueOf(userID));
+                    Log.d("lattitude", lattitude);
+                    Log.d("longitude", longitude);
+
+                    postParam.put("usersID",String.valueOf(userID));
+                    postParam.put("lattitude",lattitude);
+                    postParam.put("longitude", longitude);
+
+                    return postParam;
+                }
+
+            };
+
+            requestQueue1.add(jsonObjRequest);
+
+
+        }
+
+    }
+
     public class ViewDialog {
 
         public void showDialog(final Activity activity, String msg) {
@@ -209,7 +229,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             final Spinner typeMovement = (Spinner) findViewById(R.id.insertMovement);
             final Spinner sizeGroup = (Spinner) findViewById(R.id.insert_group_size);
             final ProgressBar spinner = (ProgressBar)findViewById(R.id.progressBar1);
-            final Button newUserbtn = (Button)  findViewById(R.id.newUserbtn);
            // final SharedPreferences prefs = this.getSharedPreferences(
                     //"com.example.williamdunnett.mapstest2", Context.MODE_PRIVATE);
 
@@ -269,6 +288,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                         protected Map<String, String> getParams() throws AuthFailureError {
 
                             Log.d("getParams new user", "called");
+                            Log.d("getParams new user", "usersName: " + nameUser);
+
                             Map<String, String> postParam = new HashMap<String, String>();
 
                             postParam.put("usersName",nameUser);
@@ -321,6 +342,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
 
     protected void getLocation() {
+        boolean gps_enabled = false, network_enabled = false;
+
         if (isLocationEnabled(MapsActivity.this)) {
             locationManager = (LocationManager)  this.getSystemService(Context.LOCATION_SERVICE);
             criteria = new Criteria();
@@ -337,6 +360,29 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 ActivityCompat.requestPermissions(MapsActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
 
             } else {
+
+                //exceptions will be thrown if provider is not permitted.
+                try{gps_enabled=locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);}catch(Exception ex){}
+                try{network_enabled=locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);}catch(Exception ex){}
+
+                //don't start listeners if no provider is enabled
+                if(!gps_enabled && !network_enabled) {
+                    Log.d("getLocation", "gps_enabled false and network_enabled false");
+                    return;
+                } else {
+                    Log.d("getLocation", "gps_enabled: " + gps_enabled + "   network_enabled: " + network_enabled);
+                }
+
+
+                if(gps_enabled)
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListenerGps);
+                if(network_enabled)
+                    locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListenerNetwork);
+
+                timer1=new Timer();
+                timer1.schedule(new GetLastLocation(), 20000);
+
+                /*
                 Location location = locationManager.getLastKnownLocation(bestProvider);
                 Log.d("getLocation", "permission has been granted");
                 if (location != null) {
@@ -355,6 +401,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     LatLng currentLocation = new LatLng(latti, longi);
                     mMap.addMarker(new MarkerOptions().position(currentLocation).title("currentLocation"));
                     mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLocation));
+                    //mMap.
 
                     addToSharedPrefsString(this, "lattitude", lattitude);
                     addToSharedPrefsString(this, "longitude", longitude);
@@ -362,7 +409,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 } else{
                     //This is what you need:
                     Log.e("TAG", "GPS is OFF");
-                   // locationManager.requestLocationUpdates(bestProvider,Long.valueOf(1000) ,Float.valueOf(0), this);
+                    locationManager.requestLocationUpdates(bestProvider,Long.valueOf(1000) ,Float.valueOf(0), locationListenerGps);
 
                     //TODO REMOVE AND MAKE PROPER WORKING CODE
                     String lattitude = "50.79365";
@@ -382,6 +429,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     addToSharedPrefsString(this, "lattitude", lattitude);
                     addToSharedPrefsString(this, "longitude", longitude);
                 }
+            */
             }
 
         }
@@ -392,6 +440,104 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             //.................
         }
     }
+
+    class GetLastLocation extends TimerTask {
+        @Override
+        public void run() {
+            Location net_loc=null, gps_loc=null;
+            Boolean gps_enabled=false, network_enabled=false;
+            locationManager.removeUpdates(locationListenerGps);
+            locationManager.removeUpdates(locationListenerNetwork);
+
+            //exceptions will be thrown if provider is not permitted.
+            try{gps_enabled=locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);}catch(Exception ex){}
+            try{network_enabled=locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);}catch(Exception ex){}
+
+            if (ActivityCompat.checkSelfPermission(MapsActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission
+                    (MapsActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                Log.d("getLastLocation", "Permission needed");
+                ActivityCompat.requestPermissions(MapsActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+
+            } else {
+                if(gps_enabled)
+                    gps_loc=locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                if(network_enabled)
+                    net_loc=locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+            }
+
+            //if there are both values use the latest one
+            if(gps_loc!=null && net_loc!=null){
+                if(gps_loc.getTime()>net_loc.getTime())
+                    Log.d("GetLastKnownLocation", "Both gps_loc: " + gps_loc.toString());
+                    //locationResult.gotLocation(gps_loc);
+                else
+                    Log.d("GetLastKnownLocation", "Both net_loc: " + net_loc.toString());
+                    //locationResult.gotLocation(net_loc);
+                return;
+            }
+
+            if(gps_loc!=null){
+                Log.d("GetLastKnownLocation", "gps_loc: " + gps_loc.toString());
+                //MapsActivity.UpdateLocation.updateServer(gps_loc);
+                return;
+            }
+            if(net_loc!=null){
+                Log.d("GetLastKnownLocation", "net_loc: " + net_loc.toString());
+                //locationResult.gotLocation(net_loc);
+                return;
+            }
+            Log.d("GetLastKnownLocation", "Null available");
+            //locationResult.gotLocation(null);
+        }
+    }
+
+    LocationListener locationListenerGps = new LocationListener() {
+        public void onLocationChanged(Location location) {
+            timer1.cancel();
+            //TODO change this line to save location in prefs
+            //locationResult.gotLocation(location);
+            Log.d("locationListenerGPS","called");
+            locationManager.removeUpdates(this);
+            locationManager.removeUpdates(locationListenerNetwork);
+        }
+        public void onProviderDisabled(String provider) {}
+        public void onProviderEnabled(String provider) {}
+        public void onStatusChanged(String provider, int status, Bundle extras) {}
+    };
+
+    LocationListener locationListenerNetwork = new LocationListener() {
+        public void onLocationChanged(Location location) {
+            timer1.cancel();
+            Log.d("locationListenerNETWORK","called");
+
+            double latti = location.getLatitude();
+            double longi = location.getLongitude();
+            //double latti = 80;
+            //double longi = 80;
+            String lattitude = String.valueOf(latti);
+            String longitude = String.valueOf(longi);
+
+            Log.d("getLocation", "Lattitude = " + lattitude +
+                    " Longitude = " + longitude);
+
+            // Add a marker in currentLocation  and move the camera
+            LatLng currentLocation = new LatLng(latti, longi);
+            mMap.addMarker(new MarkerOptions().position(currentLocation).title("currentLocation"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLocation));
+            //mMap.
+
+            //TODO change this line to save location in prefs
+            addToSharedPrefsString(MapsActivity.this, "lattitude", lattitude);
+            addToSharedPrefsString(MapsActivity.this, "longitude", longitude);
+            //locationResult.gotLocation(location);
+            //locationManager.removeUpdates(this);
+            locationManager.removeUpdates(locationListenerGps);
+        }
+        public void onProviderDisabled(String provider) {}
+        public void onProviderEnabled(String provider) {}
+        public void onStatusChanged(String provider, int status, Bundle extras) {}
+    };
 
     public static boolean isLocationEnabled(Context context)
     {
